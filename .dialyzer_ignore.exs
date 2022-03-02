070f9bc7f765599a4e04376cf02927abb7cb1efd
Dominic Letz <dominic@diode.io>
2022-03-02 18:40:38 +0100

    Credo

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,6 +1,6 @@
 [
   {"lib/diode_client/cowboy2_adapter.ex:0:unknown_function Function Plug.Cowboy.child_spec/1 does not exist."},
-  {"lib/diode_client/cowboy2_adapter.ex:79:unknown_function Function Plug.Cowboy.child_spec/1 does not exist."},
+  {"lib/diode_client/cowboy2_adapter.ex:80:unknown_function Function Plug.Cowboy.child_spec/1 does not exist."},
   {"lib/diode_client/cowboy2_adapter.ex:0:unknown_function Function :ranch_transport.sendfile/6 does not exist."},
-  {"lib/diode_client/cowboy2_adapter.ex:33:unknown_function Function :ranch_transport.sendfile/6 does not exist."}
+  {"lib/diode_client/cowboy2_adapter.ex:34:unknown_function Function :ranch_transport.sendfile/6 does not exist."}
 ]
