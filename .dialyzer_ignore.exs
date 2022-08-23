d0c2651dc6667e901adff7743c4ce3c2a6f1d68d
Dominic Letz <dominic@diode.io>
2022-08-23 12:12:32 +0200

    updated ignores

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,6 +1,6 @@
 [
   {"lib/diode_client/cowboy2_adapter.ex:0:unknown_function Function Plug.Cowboy.child_spec/1 does not exist."},
-  {"lib/diode_client/cowboy2_adapter.ex:45:unknown_function Function Plug.Cowboy.child_spec/1 does not exist."},
+  {"lib/diode_client/cowboy2_adapter.ex:41:unknown_function Function Plug.Cowboy.child_spec/1 does not exist."},
   {"lib/diode_client/transport.ex:0:unknown_function Function :ranch_transport.sendfile/6 does not exist."},
-  {"lib/diode_client/transport.ex:47:unknown_function Function :ranch_transport.sendfile/6 does not exist."}
+  {"lib/diode_client/transport.ex:49:unknown_function Function :ranch_transport.sendfile/6 does not exist."}
 ]
