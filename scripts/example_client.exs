86fdbe209a71760098986887f082f6410b34d217
Dominic Letz <dominic@diode.io>
2024-07-08 13:49:24 +0200

    Updated example client

--- scripts/example_client.exs
+++ scripts/example_client.exs
@@ -4,8 +4,10 @@ Mix.install([{:diode_client, path: "../"}])
 # Client: Below enter your server address
 server_address = "0x389eba94b330140579cdce1feb1a6e905ff876e6"
 DiodeClient.interface_add("example_client_interface")
+address = DiodeClient.Base16.encode(DiodeClient.address())
 
 spawn_link(fn ->
+  IO.puts("client #{address} started")
   {:ok, ssl} = DiodeClient.port_connect(server_address, 5000)
   :ssl.controlling_process(ssl, self())
   :ssl.setopts(ssl, packet: :line, active: true)
