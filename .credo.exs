7f240d32edbe15b8c812176bc7f81c7a514ffa73
Dominic Letz <dominic@diode.io>
2024-08-01 02:31:40 +0200

    Update credo version & config

--- .credo.exs
+++ .credo.exs
@@ -2,11 +2,15 @@
   configs: [
     %{
       name: "default",
-
-      checks: [
-        {Credo.Check.Refactor.Nesting, max_nesting: 3},
-        {Credo.Check.Refactor.CyclomaticComplexity, max_complexity: 14}
-      ]
+      checks: %{
+        extra: [
+          {Credo.Check.Refactor.Nesting, max_nesting: 3},
+          {Credo.Check.Refactor.CyclomaticComplexity, max_complexity: 14}
+        ],
+        disabled: [
+          {Credo.Check.Readability.PredicateFunctionNames, false}
+        ]
+      }
     }
   ]
 }
