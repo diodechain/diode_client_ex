f9cd4d5ab2e435028e9916409c8231d8858a9159
Dominic Letz <dominic@diode.io>
2024-12-18 17:09:02 +0100

    v1.2.6

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule DiodeClient.MixProject do
   use Mix.Project
 
-  @version "1.2.5"
+  @version "1.2.6"
   @name "Diode Client"
   @url "https://github.com/diodechain/diode_client_ex"
   @maintainers ["Dominic Letz"]
