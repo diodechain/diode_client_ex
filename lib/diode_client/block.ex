bc6ba243ea4d54e8c7480bd5238609c307beff16
Dominic Letz <dominic@diode.io>
2026-01-15 17:06:40 +0100

    Allow Block.number() call on a pure integer

--- lib/diode_client/block.ex
+++ lib/diode_client/block.ex
@@ -2,15 +2,19 @@ defmodule DiodeClient.Block do
   @moduledoc """
   Block related helper functions
   """
-  def number(block) do
+  def number(block_number) when is_integer(block_number) do
+    block_number
+  end
+
+  def number(block) when is_map(block) or is_list(block) do
     DiodeClient.Rlpx.bin2uint(block["number"])
   end
 
-  def hash(block) do
+  def hash(block) when is_map(block) or is_list(block) do
     DiodeClient.Rlpx.bin2uint(block["block_hash"])
   end
 
-  def epoch(block) do
+  def epoch(block) when is_map(block) or is_list(block) do
     if diode?(block) do
       div(number(block), 40_320)
     else
@@ -18,35 +22,35 @@ defmodule DiodeClient.Block do
     end
   end
 
-  def diode?(block) do
+  def diode?(block) when is_map(block) or is_list(block) do
     miner_signature(block) not in [nil, ""]
   end
 
-  def miner_signature(block) do
+  def miner_signature(block) when is_map(block) or is_list(block) do
     block["miner_signature"]
   end
 
-  def nonce(block) do
+  def nonce(block) when is_map(block) or is_list(block) do
     block["nonce"]
   end
 
-  def previous_block(block) do
+  def previous_block(block) when is_map(block) or is_list(block) do
     block["previous_block"]
   end
 
-  def state_hash(block) do
+  def state_hash(block) when is_map(block) or is_list(block) do
     block["state_hash"]
   end
 
-  def timestamp(block) do
+  def timestamp(block) when is_map(block) or is_list(block) do
     DiodeClient.Rlpx.bin2uint(block["timestamp"])
   end
 
-  def nano_timestamp(block) do
+  def nano_timestamp(block) when is_map(block) or is_list(block) do
     DiodeClient.Rlpx.bin2uint(block["timestamp"]) * 1_000_000_000
   end
 
-  def transaction_hash(block) do
+  def transaction_hash(block) when is_map(block) or is_list(block) do
     block["transaction_hash"]
   end
 end
