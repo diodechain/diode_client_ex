6509c0385fe30156271662a4d3d12f7ebd3491d3
Dominic Letz <dominic@diode.io>
2026-01-30 13:10:02 +0100

    Added moduledoc

--- lib/diode_client/port/relay.ex
+++ lib/diode_client/port/relay.ex
@@ -1,3 +1,6 @@
 defmodule DiodeClient.Port.Relay do
+  @moduledoc """
+  A relay is a reserved ip:port combination on a node server that can be used relay traffic between two clients.
+  """
   defstruct [:url, :port, :source_addr]
 end
